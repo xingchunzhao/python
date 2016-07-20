0ccbcca3f90704ba97221555a24ecf1c9cc7f784
867172954@qq.com <xingchunzhao>
2016-07-21 00:56:39 +0800

    app框架编写

--- webapp/www/myORM.py
+++ webapp/www/myORM.py
@@ -1,6 +1,9 @@
 #myORM.py
-import aiomysql
+import aiomysql,logging
 import asyncio, os, json, time
+
+def log(sql, args = ()):
+	logging.info('SQL: %s' % sql)
 
 
 #全局变量__pool存储
@@ -15,7 +18,7 @@ def create_pool(loop, **kw):
 	password = kw['password'],
 	db = kw['db'],
 	charset = kw.get('charset', 'utf-8'),
-	autocommit = kw.get('autocommit', True)
+	autocommit = kw.get('autocommit', True),
 	maxsize = kw.get('maxsize', 10),
 	minsize = kw.get('minsize', 1),
 	loop = loop
@@ -56,13 +59,13 @@ def execute(sql, args):
 #？？？？？？？
 def create_args_string(num):
 	L = []
-	for n in range(num)
+	for n in range(num):
 		L.append('?')
 	return ','.join(L)
 	
 #Field类，读取数据库字段
 class Field(object):
-	def __init__(self, name, column_type, primary_key, default)
+	def __init__(self, name, column_type, primary_key, default):
 		self.name = name
 		self.column_type = column_type
 		self.primary_key = primary_key
@@ -71,7 +74,7 @@ class Field(object):
 	def __str__(self):
 		return '<%s, %s : %s>' % (self.__class__.__name__, self.column_type, self.name)
 class StringField(Field):
-	def __init__(self, name = None, primary_key = Falsem, default = None, ddl = 'varchar(100)'):
+	def __init__(self, name = None, primary_key = False, default = None, ddl = 'varchar(100)'):
 		super().__init__(name, ddl, primary_key, default)
 
 class BooleanField(Field):
@@ -79,18 +82,168 @@ class BooleanField(Field):
 		super().__init__(name, 'boolean', False, default)
 
 class IntegerField(Field):
-	def __init__(self, name = None, primary_key = False, default = 0)
+	def __init__(self, name = None, primary_key = False, default = 0.0):
 		super().__init__(name, 'bigint', primary_key, default)
 		
 class FloatField(Field):
-	def __init__(self, name = None, primary_key = False, default = 0.0)
+	def __init__(self, name = None, primary_key = False, default = 0.0):
 		super().__init__(name, 'real', primary_key, default)
 
 class TextField(Field):
 	def __init__(self, name = None, default = None):
 		super().__init__(name, 'text', False, default)
-			
 		
+#定义ORM，主要是创建ModelMetaclass类
+#Model只是一个基类，必须要通过metaclass类将子类的User信息读取出来
+
+class ModelMetaclass(type):
+	
+	def __new__(cls, name, bases, attrs):
+		#排除Model类本身：
+		if name == 'Model':
+			return type.__new__(cls, name, bases, attrs)
+		
+		#获取table名称：
+		tableName = attrs.get('__table__', None) or name
+		logging.info('found model: %s (table:%s)' % (name, tableName))
+		
+		#获取所有的Field和主键名：
+		mappings = dict()
+		fields = []
+		primaryKey = None
+		for k, v in attrs.items():
+			if isinstance(v, Field):
+				logging.info('found mapping: %s ==> %s' % (k, v))
+				mappings[k] = v
+				if v.primary_key:  #找到主键
+					if primaryKey:
+						raise RuntimeError('Duplicate primary key for field:%s' % k)
+					primaryKey = k
+				else:
+					fields.append(k)
+		if not primaryKey:
+			raise RuntimeError('Primary key not found')
+		for k in mappings.keys():
+			attrs.pop(k)
+		escaped_fields = list(map(lambda f: '%s' % f, fields))
+		attrs['__mappings__'] = mappings #保持属性和列的映射关系
+		attrs['__table_'] = tableName
+		attrs['__primary_key__'] = primaryKey #主键属性名
+		attrs['__fields__'] = fields #除主键外的属性名
+		#构造默认的Select, Insert, Update, Delte语句
+		attrs['__select__'] = 'select %s , %s from %s ' % (primaryKey, ','.join(escaped_fields), tableName)
+		attrs['__insert__'] = 'insert into %s (%s, %s) values (%s)' % (tableName, ','.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+		#attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+		attrs['__update__'] = 'update %s, set %s where %s = ?' % (tableName, ','.join(map(lambda f : '%s = ?' % (mappings.get(f).name or f), fields)), primaryKey)
+		attrs['__delete__'] = 'delete from %s where %s = ?' % (tableName, primaryKey)
+		return type.__new__(cls, name, bases, attrs)
+		
+
+
+#定义Model，明确ORM映射
+class Model(dict, metaclass = ModelMetaclass):
+	
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+	
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+	
+	def __setattr__(self, key, value):
+		self[key] = value
+	
+	def __getValue__(self, key):
+		return getattr(self, key, None)
+		
+	def getValueOrDefault(self, key):
+		value = getattr(self, key, None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s:%s' % (key, str(value)))
+	
+	#往Model类添加class方法，让所有的子类调用class方法
+	@classmethod
+	@asyncio.coroutine
+	def findAll(cls, where = None, args = None, **kw):
+		'find object by where clause.'
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = kw.get('orderBy', None)
+		if orderBy:
+			sql.append('order by')
+			sql.append(orderBy)
+		limit = kw.get('limit', None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit, int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit, tuple) and len(limit) == 2:
+				sql.append('?,?')
+				args.append(limit)
+			else:
+				raise ValueError('Invalid limit value: %s ' % str(limit ))
+		rs = yield from select(''.join(sql), args)
+		return [cls(**r) for r in rs]
+	
+	@classmethod
+	@asyncio.coroutine
+	def findNumber(cls, selectField, where = None, args = None):
+		'find number by select and where.'
+		sql = ['select %s _num_ from %s' % (selectField, cls.__table_)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = yield from select(''.join(sql), args, 1)
+		if len(rs) == 0:
+			return None
+		return rs[0]['_num_']
+	
+	@classmethod
+	@asyncio.coroutine
+	def find(cls, pk):
+		'find object by primary key.'
+		rs = yield from select('%s where %s = ?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+		
+	
+	#往Model类添加实例方法，让所有的子类调用实例方法
+	@asyncio.coroutine
+	def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		args.append(self.getValueOrDefault(self.__primary_key__))
+		rows = yield from execute(self.__insert__, args)
+		if rows != 1:
+			logging.warn('field to insert record: affected rows:%s ' % rows)
+	
+	@asyncio.coroutine
+	def update(self):
+		args = list(map(self.getValue, self._fields_))
+		args.append(self.getValue(self.__primary_key__))
+		rows = yield from execute(self.__update__, args)
+		if rows != 1:
+			logging.warn('faild to update by primary key: affected rows: %s' % rows)
+	
+	@asyncio.coroutine
+	def remove(self):
+		args = [self.getValue(self.__primary_key__)]
+		rows = yield from execute(self.__delete__, args)
+		if rows != 1:
+			logging.warn('faild to remove by primary key:affected rows: %s' % rows)
+	
+	
+			
 			
 
 	
--- webapp/www/myUser.py
+++ webapp/www/myUser.py
@@ -1,9 +1,30 @@
 #myUser.py
 
-from myORM import Model, StringField, IntegerField
+import myORM
+from myModel import User, Blog, Comment
+import asyncio, aiomysql
+import sys
 
-class User():
-	__table__ == '__users__'
+#class User():
+	#__table__ == '__users__'
 	
-	id = IntegerField(primary_key = True)
-	name = StringField()
+	#id = IntegerField(primary_key = True)
+	#name = StringField()
+
+
+def test(loop):
+	yield from myORM.create_pool(loop = loop, host = '127.0.0.1', port = '3306', user = 'www-data', password = 'www-data', db = 'mypython')
+	u = User(name = 'Test', email = 'test@example.com', password = '1234567890', image = 'about:block' )
+	
+	yield from u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
+if loop.is_closed():
+	sys.exit()
+
+	
+#for x in test():
+#	pass
+	
